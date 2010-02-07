720151f7842dcb0f552fb6fbaac9e6f10fdda30d
Paddy Mullen <paddy@paddy-t500.(none)>
2010-02-07 00:35:30 -0500

    rewrote example-usage to have better variable names, real html element and css properties, and more lisp like syntax

--- example-usage.lisp
+++ example-usage.lisp
@@ -4,96 +4,101 @@
 (css (("#foo") (:height "50px")))
 "
 #foo {
-bar:50px;
-}"
+height:50px;
+}
+"
 
 ;; defining a css-variable manually
 
-(setq my-css-var (list :inside-var "50px 30px"))
+(setq my-css-var '(:margin "50px 30px"))
 (setf (get 'my-css-var 'css-var) t)
 
 ;; using that css-variable
-(css (("#foo") (:bar "50px" my-css-var)))
+(css (("#foo") (:height "50px" my-css-var)))
 "
 #foo {
-bar:50px;
-inside-var:50px 30px;
-}"
+height:50px;
+margin:50px 30px;
+}
+"
 
 ;; now defining a css-variable with the make-css-var macro
 
-(make-css-var my-macroed-css-var '(:border "1px solid red"))
+(make-css-var my-favorite-border-var '(:border "1px solid red"))
 
 ;;using that variable
-(css (("#foo") (:bar "50px" my-css-var my-macroed-css-var)))
+(css (("#foo") (:height "50px" my-css-var my-favorite-border-var)))
 "
 #foo {
-bar:50px;
-inside-var:50px 30px;
+height:50px;
+margin:50px 30px;
 border:1px solid red;
-}"
+}
+"
 
 
 ;; now with cascading 
-(css (
-      ("#foo") (:bar "50px" my-css-var my-macroed-css-var)  
-      (("#foo") (:bar "50px" my-css-var my-macroed-css-var))))
+      ("#foo") (:length "50px" my-css-var my-macroed-css-var)  
+      (("#foo") (:width "50px" :float "left"  my-css-var my-macroed-css-var))))
+
 "
 #foo {
-bar:50px;
-inside-var:50px 30px;
+length:50px;
+margin:50px 30px;
 border:1px solid red;
 }
 
 #foo #foo {
-bar:50px;
-inside-var:50px 30px;
+width:50px;
+float:left;
+margin:50px 30px;
 border:1px solid red;
 }
 "
+
 ;;now with functions
 (make-css-func foo-func2 (avar) (list avar avar))
 
 (css (
-      ("#foo") ((foo-func2 "should-be-repeated") :bar "50px" my-css-var my-macroed-css-var)  
-      (("#foo") (:bar "50px" my-css-var my-macroed-css-var))))
+      ("#foo") ((foo-func2 "should-be-repeated") :height "50px" my-css-var my-favorite-border-var)
+      (("li") (:width "50px" :float "left"  my-css-var my-favorite-border-var))))
 
 "
 #foo {
 should-be-repeated:should-be-repeated;
-bar:50px;
-inside-var:50px 30px;
+height:50px;
+margin:50px 30px;
 border:1px solid red;
 }
 
-#foo #foo {
-bar:50px;
-inside-var:50px 30px;
+#foo li {
+width:50px;
+float:left;
+margin:50px 30px;
 border:1px solid red;
 }
 "
 ;; now with comments
 (make-css-func comment (comment-string) (list (concatenate 'string "/*" comment-string) "*/"))
 (css (
-      ("#foo") ((comment "a comment" ) (foo-func2 "should-be-repeated") :bar "50px" my-css-var my-macroed-css-var)  
-      (("#foo") (:bar "50px" my-css-var my-macroed-css-var))))
+      ("#foo") ((comment "a comment" ) (foo-func2 "should-be-repeated") :height "50px" my-css-var my-favorite-border-var)
+      (("li") (:width "50px" my-css-var my-favorite-border-var))))
 
 "
 #foo {
 /*a comment:*/;
 should-be-repeated:should-be-repeated;
-bar:50px;
-inside-var:50px 30px;
+height:50px;
+margin:50px 30px;
 border:1px solid red;
 }
 
-#foo #foo {
-bar:50px;
-inside-var:50px 30px;
+#foo li {
+width:50px;
+margin:50px 30px;
 border:1px solid red;
 }
 "
-
 
 ;; note this is a bit of a hack
 ;;
